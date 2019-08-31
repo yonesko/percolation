5ae923bcf6e957e4dbfc5a43b9f28e7fc0b9107d
Gleb <yonesko@gmail.com>
2019-08-31 20:43:28 +0300

    backwash

--- Percolation.java
+++ Percolation.java
@@ -8,7 +8,8 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private final WeightedQuickUnionUF weightedQuickUnionUF;
+    private final WeightedQuickUnionUF perculationUF;
+    private final WeightedQuickUnionUF fullnessUF;
     private final int n;
     private final boolean[] opened;
     private int numberOfOpenSites;
@@ -18,10 +19,12 @@ public class Percolation {
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
         this.n = n;
-        weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
+        perculationUF = new WeightedQuickUnionUF(n * n + 2);
+        fullnessUF = new WeightedQuickUnionUF(n * n + 2);
         for (int i = 1; i <= n; i++) {
-            weightedQuickUnionUF.union(0, index(1, i));
-            weightedQuickUnionUF.union(index(n, n) + 1, index(n, i));
+            fullnessUF.union(0, index(1, i));
+            perculationUF.union(0, index(1, i));
+            perculationUF.union(index(n, n) + 1, index(n, i));
         }
         opened = new boolean[n * n];
     }
@@ -33,14 +36,22 @@ public class Percolation {
 
         numberOfOpenSites++;
         opened[indexOpened(row, col)] = true;
-        if (row + 1 <= n && isOpen(row + 1, col))
-            weightedQuickUnionUF.union(index(row, col), index(row + 1, col));
-        if (row - 1 >= 1 && isOpen(row - 1, col))
-            weightedQuickUnionUF.union(index(row, col), index(row - 1, col));
-        if (col + 1 <= n && isOpen(row, col + 1))
-            weightedQuickUnionUF.union(index(row, col), index(row, col + 1));
-        if (col - 1 >= 1 && isOpen(row, col - 1))
-            weightedQuickUnionUF.union(index(row, col), index(row, col - 1));
+        if (row + 1 <= n && isOpen(row + 1, col)) {
+            perculationUF.union(index(row, col), index(row + 1, col));
+            fullnessUF.union(index(row, col), index(row + 1, col));
+        }
+        if (row - 1 >= 1 && isOpen(row - 1, col)) {
+            perculationUF.union(index(row, col), index(row - 1, col));
+            fullnessUF.union(index(row, col), index(row - 1, col));
+        }
+        if (col + 1 <= n && isOpen(row, col + 1)) {
+            perculationUF.union(index(row, col), index(row, col + 1));
+            fullnessUF.union(index(row, col), index(row, col + 1));
+        }
+        if (col - 1 >= 1 && isOpen(row, col - 1)) {
+            perculationUF.union(index(row, col), index(row, col - 1));
+            fullnessUF.union(index(row, col), index(row, col - 1));
+        }
     }
 
     private int index(int row, int col) {
@@ -65,7 +76,7 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validateCoords(row, col);
-        return isOpen(row, col) && weightedQuickUnionUF.connected(0, index(row, col));
+        return isOpen(row, col) && fullnessUF.connected(0, index(row, col));
     }
 
     // returns the number of open sites
@@ -75,7 +86,7 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return numberOfOpenSites > 0 && weightedQuickUnionUF.connected(0, index(n, n) + 1);
+        return numberOfOpenSites > 0 && perculationUF.connected(0, index(n, n) + 1);
     }
 
     // test client (optional)
